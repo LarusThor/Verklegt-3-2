11e7a6b0e7c133cd5fdebd3e249881722fa6ecec
LarusThor <142886101+LarusThor@users.noreply.github.com>
2024-12-03 10:39:00 +0000

    push

---
+++ TheContactor/src/components/Counter/counter.jsx
@@ -0,0 +1,26 @@
+import { View, Button, Text } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { incrementCounter } from '../../redux/features/counter/counter-slice';
+
+export function Counter() {
+
+    const dispatch = useDispatch();
+    const counter = useSelector((state) => {
+        console.log(state);
+        return state.counter.value;
+    });
+
+
+    return (
+
+        <View>
+            <Text>{counter}</Text>
+            <Button title="Increment" onPress={() => dispatch(incrementCounter())}>Increment +</Button>
+        </View>
+
+    );
+
+};
+
+
+
