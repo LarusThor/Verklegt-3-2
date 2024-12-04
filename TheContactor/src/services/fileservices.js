8022c2dc991d08f11e1dc50490d304210a1102c3
LarusThor <142886101+LarusThor@users.noreply.github.com>
2024-12-04 14:01:08 +0000

    getAllContacts

--- TheContactor/src/services/fileservices.js
+++ TheContactor/src/services/fileservices.js
@@ -2,6 +2,7 @@ import * as FileSystem from 'expo-file-system';
 const contactDirectory = `${FileSystem.documentDirectory}contacts`;
 
 const createContact = async (contactInfo) => {
+
     const content = JSON.stringify({
           'contact': { 
               "id": contactInfo.id,
@@ -18,6 +19,31 @@ const createContact = async (contactInfo) => {
     const fileContents = await FileSystem.readAsStringAsync(filePath)
     console.log(`New contact: ${fileContents}`)
 
+};
+
+const setupDirectory = async () => {
+    const dir = await FileSystem.getInfoAsync(contactDirectory);
+    if (!dir.exists) await FileSystem.makeDirectoryAsync(contactDirectory);
+};
+
+export const getAllContacts = async () => {
+    await setupDirectory();
+    const result = await FileSystem.readDirectoryAsync(contactDirectory);
+    console.log('results: ', result)
+    const contacts = await Promise.all(
+        result.map(async (contactFileName) => {
+            const filepath = `${contactDirectory}/${contactFileName}`;
+            const fileContents = await FileSystem.readAsStringAsync(filepath);
+            return JSON.parse(fileContents);
+        })
+    );
+
+    return contacts
+    // result.map(async contact => {
+    //     return {
+    //         contact
+    //     }
+    // }) 
 };
 
 export default createContact;
--- TheContactor/src/views/ContactList/index.jsx
+++ TheContactor/src/views/ContactList/index.jsx
@@ -3,10 +3,14 @@ import { View, Text, TouchableHighlight } from 'react-native';
 import style from './styles';
 import SearchBar from '../../components/SearchBar';
 import Contact from '../../components/Contact';
-
+import { getAllContacts } from '../../services/fileservices';
 
 
 const ContactList = ( {navigation: { navigate } } ) => {
+
+    
+    const contacts = getAllContacts()
+    console.log(contacts)
     return (
         <View style={style.container}>
             <View>
